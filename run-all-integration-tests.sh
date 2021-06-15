154a6eac5428c07ab5c51b9ce5b36110ef01d8fd
unknown <draganculibrk9@gmail.com>
2021-06-15 22:11:43 +0200

    #63 Update script

--- run-all-integration-tests.sh
+++ run-all-integration-tests.sh
@@ -31,9 +31,9 @@ while ! is_finished agent-products; do sleep 20; done
 while ! is_finished agent-reports; do sleep 20; done
 while ! is_finished agent-invoices; do sleep 20; done
 
-AGENT_PRODUCTS_TEST_EXIT_CODE=return_exit_code agent-products
-AGENT_REPORTS_TEST_EXIT_CODE=return_exit_code agent-reports
-AGENT_INVOICES_TEST_EXIT_CODE=return_exit_code agent-invoices
+AGENT_PRODUCTS_TEST_EXIT_CODE="$(return_exit_code agent-products)"
+AGENT_REPORTS_TEST_EXIT_CODE="$(return_exit_code agent-reports)"
+AGENT_INVOICES_TEST_EXIT_CODE="$(return_exit_code agent-invoices)"
 
 echo "agent-products tests returned $AGENT_PRODUCTS_TEST_EXIT_CODE"
 echo "agent-reports tests returned $AGENT_REPORTS_TEST_EXIT_CODE"
