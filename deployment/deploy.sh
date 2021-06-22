ea49bc5c05b4e92dfed16b2f648db5d71c21cf67
Danijel Radulović <master.daca09@gmail.com>
2021-06-22 12:22:58 +0200

    Create scripts and workflow for deploying agent services on Heroku

---
+++ deployment/deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+HEROKU_EMAIL=${1}
+HEROKU_API_KEY=${2}
+VERSION=${3}
+TERRAFORM_PG_BACKEND=${4}
+NAME=${5:-staging}
+DOCKERHUB_USERNAME=${6:-goveed20}
+CONTAINER_NAME=${7:-terraform-deploy}
+
+APP_NAME_AGENT_PRODUCTS=agentproducts
+APP_NAME_AGENT_INVOICES=agentinvoices
+APP_NAME_AGENT_REPORTS=agentreports
+
+APP_IMAGE_NAME_AGENT_PRODUCTS=${DOCKERHUB_USERNAME}/${APP_NAME_AGENT_PRODUCTS}:${VERSION}
+APP_IMAGE_NAME_AGENT_INVOICES=${DOCKERHUB_USERNAME}/${APP_NAME_AGENT_INVOICES}:${VERSION}
+APP_IMAGE_NAME_AGENT_REPORTS=${DOCKERHUB_USERNAME}/${APP_NAME_AGENT_REPORTS}:${VERSION}
+
+
+APP_NAME_AGENT_PRODUCTS_HEROKU=agentproducts${NAME}
+APP_NAME_AGENT_INVOICES_HEROKU=agentinvoices${NAME}
+APP_NAME_AGENT_REPORTS_HEROKU=agentreports${NAME}
+# create can be used to create container from base image
+# add new files, env variables
+# and then in separete comand to start container
+
+# here we use it for running terraform scripts to deploy on Heroku staging and prod env
+# we override CMD but ENTRYPOINT probably does heroku login
+DOCKER_BUILDKIT=1 docker create \
+  --workdir /deployment \
+  --entrypoint sh \
+  --env APP_IMAGE_NAME_AGENT_PRODUCTS="${APP_IMAGE_NAME_AGENT_PRODUCTS}" \
+  --env APP_IMAGE_NAME_AGENT_INVOICES="${APP_IMAGE_NAME_AGENT_INVOICES}" \
+  --env APP_IMAGE_NAME_AGENT_REPORTS="${APP_IMAGE_NAME_AGENT_REPORTS}" \
+  --env APP_NAME_AGENT_PRODUCTS="${APP_NAME_AGENT_PRODUCTS_HEROKU}" \
+  --env APP_NAME_AGENT_INVOICES="${APP_NAME_AGENT_INVOICES_HEROKU}" \
+  --env APP_NAME_AGENT_REPORTS="${APP_NAME_AGENT_REPORTS_HEROKU}" \
+  --env HEROKU_API_KEY="${HEROKU_API_KEY}" \
+  --env HEROKU_EMAIL="${HEROKU_EMAIL}" \
+  --env TERRAFORM_PG_BACKEND="${TERRAFORM_PG_BACKEND}" \
+  --name "$CONTAINER_NAME" \
+  danijelradakovic/heroku-terraform \
+  installBash.sh
+
+docker cp deployment/. "${CONTAINER_NAME}":/deployment/
+docker start -i "${CONTAINER_NAME}"
+docker rm "${CONTAINER_NAME}"
---
+++ deployment/deployment/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# CREATE TERRAFORM BACKEND ON HEROKU POSTGRES
+# heroku create $TERRAFORM_PG_BACKEND
+# heroku addons:create heroku-postgresql:hobby-dev --app $TERRAFORM_PG_BACKEND
+
+ALL_HEROKU_APPS=$(heroku apps) && export ALL_HEROKU_APPS
+
+echo $ALL_HEROKU_APPS
+echo $TERRAFORM_PG_BACKEND
+
+if [[ $ALL_HEROKU_APPS =~ "$TERRAFORM_PG_BACKEND" ]]; 
+then
+  echo "It's there!"
+else 
+  echo "It's no there!"
+  heroku create $TERRAFORM_PG_BACKEND
+  heroku addons:create heroku-postgresql:hobby-dev --app $TERRAFORM_PG_BACKEND
+fi
+
+
+cd terraform || exit
+DATABASE_URL=$(heroku config:get DATABASE_URL --app "$TERRAFORM_PG_BACKEND") && export DATABASE_URL
+
+
+# prepare Dockerfile-s
+
+rm -rf ./agent-products/Dockerfile
+rm -rf ./agent-invoices/Dockerfile
+rm -rf ./agent-reports/Dockerfile
+
+
+echo "FROM $APP_IMAGE_NAME_AGENT_PRODUCTS" >> ./agent-products/Dockerfile
+cat ./agent-products/Dockerfile
+
+echo "FROM $APP_IMAGE_NAME_AGENT_INVOICES" >> ./agent-invoices/Dockerfile
+cat ./agent-invoices/Dockerfile
+
+echo "FROM $APP_IMAGE_NAME_AGENT_REPORTS" >> ./agent-reports/Dockerfile
+cat ./agent-reports/Dockerfile
+
+terraform init -backend-config="conn_str=$DATABASE_URL"
+
+echo "Connected to database"
+
+terraform apply -auto-approve -var agent-products-name="$APP_NAME_AGENT_PRODUCTS" \
+                              -var agent-invoices-name="$APP_NAME_AGENT_INVOICES"   \
+                              -var agent-reports-name="$APP_NAME_AGENT_REPORTS"
