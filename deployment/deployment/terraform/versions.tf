1a70d1dfd93acba0c93462ff2c77c08566f518f4
Danijel Radulović <master.daca09@gmail.com>
2021-06-22 17:54:44 +0200

    #70 Update versions.tf

--- deployment/deployment/terraform/versions.tf
+++ deployment/deployment/terraform/versions.tf
@@ -3,6 +3,10 @@ terraform {
     heroku = {
       source = "heroku/heroku"
     }
+    mysql = {
+      source  = "winebarrel/mysql"
+      version = "~> 1.10.2"
+    }
   }
   required_version = ">= 0.13"
 }
